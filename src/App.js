9a5b58c46942a7ae54da5f0f3b30e5dd064db66f
oneEyedSunday <idiakosesunday@gmail.com>
2018-12-07 13:16:43 +0100

    Updated App component structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import components from './components';
+
+const { Footer, NavBar, Home } = components;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
+      <div className="container">
+          <NavBar />
+          <main className="main flex-column justify-content__center align-items__center">
+            <Home />
+          </main>
+          <Footer />     
       </div>
     );
   }
