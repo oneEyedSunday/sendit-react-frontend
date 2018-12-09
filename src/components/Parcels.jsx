638d6d53994a71c3c4f95cdc4e6aa57e37c52faf
oneEyedSunday <idiakosesunday@gmail.com>
2018-12-09 18:34:09 +0100

    Implement nested routing in Parcels component

--- src/components/Parcels.jsx
+++ src/components/Parcels.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 
 import uielems from '../uielements';
+import ParcelDetailsComponent from './ParcelDetails';
 
 import './parcels.css';
 
@@ -30,21 +32,31 @@ export default class Parcels extends Component {
     constructor() {
         super();
         this.state = {
-            parcels: []
+            parcels: dummyParcels
         }
 
         this.headers = ['S/N', 'Date Created', 'Destination', 'Delivery Status', 'Options'];
     }
+
     render() {
         return (
-            <React.Fragment>
-                <div className="parcel__context">
-                    <p>Your parcel delivery orders
-                        <a href="/create" className="action link" title="Create Order"> &#43; </a>
-                    </p>
-                </div>
-                <Table  headers={this.headers} data={dummyParcels}/>
-            </React.Fragment>
+            <Router>
+                <Switch>
+                    <Route  path="/parcels/details/:id" render={() => <ParcelDetailsComponent parcels={this.state.parcels}/>}/>
+                    <Route path="**"  render={
+                        () => (
+                            <React.Fragment>
+                                <div className="parcel__context">
+                                    <p>Your parcel delivery orders
+                                        <a href="/create" className="action link" title="Create Order"> &#43; </a>
+                                    </p>
+                                </div>
+                                <Table  headers={this.headers} data={this.state.parcels}/>
+                            </React.Fragment>
+                        )
+                    } />
+                </Switch>
+            </Router>
         )
     }
 }
