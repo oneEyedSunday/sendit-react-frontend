5ce06688d4a16c9bec28ea29fc21ac041f9146f6
oneEyedSunday <idiakosesunday@gmail.com>
2018-12-09 11:45:10 +0100

    Bootstrapped needed files for Parcels

---
+++ src/components/Parcels.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Parcels extends Component {
+    constructor() {
+        super();
+        this.state = {
+            parcels: []
+        }
+    }
+    render() {
+        return (
+            <div>Parcels</div>
+        )
+    }
+}
--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ import Home from './Home';
 import NavBar from './NavBar';
 import Footer from './Footer';
 import Auth from './Auth';
+import Parcels from './Parcels';
 
 export default {
     Home,
     NavBar,
     Footer,
-    Auth
+    Auth,
+    Parcels
 };
